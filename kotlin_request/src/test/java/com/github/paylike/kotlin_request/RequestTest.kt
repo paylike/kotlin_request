7b171c3ff9bc47f9639222f76d4ae85df1f89065
Imre 'Rover' Racz <racz.imre.levente@gmail.com>
2022-06-08 21:50:48 +0200

    test(requester): added test for the rest of the functionality

--- kotlin_request/src/test/java/com/github/paylike/kotlin_request/RequestTest.kt
+++ kotlin_request/src/test/java/com/github/paylike/kotlin_request/RequestTest.kt
@@ -9,9 +9,60 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class RequestTest {
+    @Test
+    fun paylike_requester_form() {
+        val app: HttpHandler = fun(req: Request): Response {
+            assertEquals(Method.POST, req.method)
+            val headers = req.headers.toMap()
+            assertEquals("1", headers.getValue("Accept-Version"))
+            assertEquals("kotlin-1", headers.getValue("X-Client"))
+            assertEquals("application/x-www-form-urlencoded", headers.getValue("Content-Type"))
+            assertEquals("foo=bar", req.bodyString())
+            assertEquals("${"foo=bar".length}", headers.getValue("Content-Length"))
+            return Response(Status.OK)
+        }
+        val jettyServer = app.asServer(Jetty(9000)).start()
+        val opts = RequestOptions(
+            form = true,
+            formFields = mapOf("foo" to "bar")
+        )
+        val requester = PaylikeRequester()
+        val response = requester.request(
+            endpoint = "http://localhost:9000",
+            opts = opts
+        )
+        assertEquals(200, response.response.status.code)
+        jettyServer.close()
+    }
+
 
     @Test
-    fun paylike_requester() {
+    fun paylike_requester_get() {
+        val app: HttpHandler = fun(req: Request): Response {
+            assertEquals(Method.GET, req.method)
+            val headers = req.headers.toMap()
+            assertEquals("1", headers.getValue("Accept-Version"))
+            assertEquals("kotlin-1", headers.getValue("X-Client"))
+            assertEquals("bar1", req.query("foo1"))
+            assertEquals("bar2", req.query("foo2"))
+            return Response(Status.OK)
+        }
+        val jettyServer = app.asServer(Jetty(9000)).start()
+        val opts = RequestOptions(
+            query = mapOf("foo1" to "bar1", "foo2" to "bar2"),
+            method = "GET",
+        )
+        val requester = PaylikeRequester()
+        val response = requester.request(
+            endpoint = "http://localhost:9000",
+            opts = opts
+        )
+        assertEquals(200, response.response.status.code)
+        jettyServer.close()
+    }
+
+    @Test
+    fun paylike_requester_post() {
         val app: HttpHandler = fun(req: Request): Response {
             assertEquals(Method.POST, req.method)
             val headers = req.headers.toMap()
